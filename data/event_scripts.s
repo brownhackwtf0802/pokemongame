1d1582304aa9b4ea2d33669d5b861313b88398c9
brownhackwtf0802 <dennis.mensing@live.de>
2025-08-31 00:47:41 +0200

    Porymap things
    Porymap Dinge :D Klappt alles, compiled noch

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1158,3 +1158,9 @@ EventScript_VsSeekerChargingDone::
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
 	.include "data/scripts/dexnav.inc"
+
+	.include "data/maps/StartTown/scripts.inc"
+
+	.include "data/maps/Route1/scripts.inc"
+
+	.include "data/maps/StartTown_ProfessorsLab/scripts.inc"
